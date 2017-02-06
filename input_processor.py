71c2671757d3b51ae8cc15330d35376d4199648a
willtop <billcuitop@yahoo.com>
2017-02-05 21:59:37 -0500

    Revised input OCR, rectifying the noise of string detection.

--- input_processor.py
+++ input_processor.py
@@ -1,3 +1,8 @@
+# First of all, introduce pysseract into the python path
+#import sys
+#sys.path.append('C:\\Users\\happy\\AppData\\Local\\Tesseract-OCR')
+#print("Done setting pysseract script into path!")
+
 import cv2
 import pytesseract
 from PIL import Image, ImageGrab, ImageEnhance, ImageFilter
@@ -5,15 +10,20 @@ import numpy as np
 
 # pytesseract.pytesseract.tesseract_cmd = 'pytesser_v0.0.1/tesseract.exe'
 
+# define a global variable record the current score 
+current_score = 0
+
 class GameState:
 
-    bounding_box = (0, 107, 562, 1080)
+    # bounding_box: original setting 0, 107, 562, 1080
+    bounding_box = (0, 0, 350, 600)
     box_width = bounding_box[2] - bounding_box[0]
     box_height = bounding_box[3] - bounding_box[1]
 
     def __init__(self, dims):
         self.dims = dims
-        self.score_box = (200, 100, 400, 200)
+		# score_box: original setting 200, 100, 400, 200
+        self.score_box = (140, 120, 200, 195)
 
     def start(self):
         return
@@ -26,6 +36,9 @@ class GameState:
         if sum(input_vec) != 1:
             raise ValueError('Multiple input actions!')
 
+        # reference to a global variable
+        global current_score
+		
         reward = 0
         terminal = False
 
@@ -37,11 +50,46 @@ class GameState:
         score_im = raw_im.crop(self.score_box)
         score_im = score_im.filter(ImageFilter.SHARPEN)
         enhancer = ImageEnhance.Contrast(score_im)
-        score_im = enhancer.enhance(10)
+        score_im = enhancer.enhance(20)
         score_im = Image.fromarray(cv2.cvtColor(np.asarray(score_im), cv2.COLOR_RGB2GRAY))
         score = pytesseract.image_to_string(score_im, config='-psm 7 digits_only')
         print(score)
+        #print("try convert score")
+        #print(type(score))
+		
+		# The score is always confirmed to be with str type
+		# Try to convert into integer type, upon exception don't do anything
+        polished_score = []
+        final_score = []
+		
+		# As 1 is always confused with I, convert it as pre-processing
+        for i in range(len(score)):
+            if(score[i] =='I'):
+			    # detect a 1 here
+                polished_score.append(1)
+            else:
+                polished_score.append(score[i])
+		
+        for raw_digit in polished_score:
+            try:
+                digit = int(raw_digit)
+                final_score.append(digit)
+                # always check if it reads one point increase
+                if(int(''.join(map(str, final_score))) == current_score+1):
+                    # we have reached a promising reading
+                    break					
+            except ValueError:
+                pass
 
+        # if obtaining a valid score:
+        if(len(final_score) > 0):
+            if(int(''.join(map(str, final_score))) > current_score and int(''.join(map(str, final_score))) <= current_score+3):
+                current_score = int(''.join(map(str, final_score)))
+			
+        print(current_score)		
+        
+		 
+        # render the displays
         if True:
             cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('screen', self.dims[0]*3, self.dims[1]*3)
@@ -54,7 +102,7 @@ class GameState:
 
 
 if __name__ == '__main__':
-    gameState = GameState((80, 120))
+    gameState = GameState((120, 180))
 
     while True:
         gameState.frame_step([0, 1])
