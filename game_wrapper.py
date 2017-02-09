5835375239a3233add383e160c9a73e221ab4b55
Daniel Son <danielsonjh@hotmail.com>
2017-02-09 14:20:25 -0500

    Added game wrapper.

---
+++ game_wrapper.py
@@ -0,0 +1,50 @@
+import cv2
+
+import numpy as np
+from PIL import Image, ImageGrab
+from output_processor import output_processor
+
+
+class Game:
+    RENDER_DISPLAY = True
+    bounding_box = (0, 0, 350, 600)
+    box_width = bounding_box[2] - bounding_box[0]
+    box_height = bounding_box[3] - bounding_box[1]
+
+    def __init__(self, dims, tap_position, restart_tap_position):
+        self.dims = dims
+        self.tap_position = tap_position
+        self.restart_tap_position = restart_tap_position
+        self.output_processor = output_processor
+
+    def frame_step(self, input_vec):
+        if sum(input_vec) != 1:
+            raise ValueError('Multiple input actions!')
+
+        raw_im = ImageGrab.grab(bbox=self.bounding_box)
+        im = raw_im.resize(self.dims, Image.ANTIALIAS)
+        screenshot = np.asarray(im)
+
+        chosen_action = np.argmax(input_vec)
+        actions = {
+            0: lambda: None,
+            1: output_processor.tap(self.tap_position[0], self.tap_position[1])
+        }
+        actions[chosen_action]()
+
+        terminal = self.__check_terminal_state(screenshot)
+        if terminal:
+            self.__restart()
+
+        if self.RENDER_DISPLAY:
+            cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('screen', self.dims[0] * 3, self.dims[1] * 3)
+            cv2.imshow('screen', cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR))
+
+        return screenshot, terminal
+
+    def __restart(self):
+        output_processor.tap(self.restart_tap_position[0], self.restart_tap_position[1])
+
+    def __check_terminal_state(self, screenshot):
+        return False
--- output_processor.py
+++ output_processor.py
@@ -1,9 +1,10 @@
 from output_monkeyrunner_python import OutputMonkeyrunnerPython
 from output_arduino import OutputArduino
 
-output = OutputMonkeyrunnerPython()
-# output = OutputArduino()
+output_processor = OutputMonkeyrunnerPython()
+# output_processor = OutputArduino()
 
-for i in range(0, 2):
-    output.tap(200, 100)
+if __name__ == '__main__':
+    for i in range(0, 2):
+        output_processor.tap(200, 100)
 
